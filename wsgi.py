d74196dc7ad24f543f8222b360bb148b5c1cdec2
Steven Serrata <sserrata@paloaltonetworks.com>
2018-03-27 10:47:38 -0500

    Remove WSGI and INI files

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-"""Called by Green Unicorn to run API Explorer"""
-
-import os
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-from app import app
-
-
-if __name__ == "__main__":
-    app.run()
