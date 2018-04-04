be73ce18c2e19795b8e4b68c3f981477cca32fe0
Steven Serrata <sserrata@paloaltonetworks.com>
2018-04-04 18:12:45 -0500

    Update API Explorer

--- app/views.py
+++ app/views.py
@@ -987,10 +987,16 @@ def update():
         copyfile(appdb, '/opt/apiexplorer/app/db/app.json')
         copyfile(securitydb, '/opt/apiexplorer/app/db/security.json')
         os.remove('/tmp/apiexplorer.zip')
-        os.remove(old)
+        os.rmdir(old)
+        import subprocess
+        user = os.getlogin()
+        subprocess.call(
+            ["/usr/bin/chown", "-R", "{user}:{user}".format(user=user),
+             current], shell=False
+        )
         return render_template(
             'pages/updates.html',
-            msg="Done",
+            msg="Success",
             status="success"
         )
 
