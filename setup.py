944c1762c95ce2aca46c19855d0848fb23131035
Steven Serrata <sserrata@paloaltonetworks.com>
2018-04-11 15:16:45 -0500

    Add find_packages()

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="api_explorer",
-    packages=["api_explorer"],
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "rundevserver = api_explorer.main:runserver",
